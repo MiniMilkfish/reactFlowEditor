8756d6e5037251a2390a87e59d0eeb1c87a0303c
MiniMilkFish <minimilkfish@outlook.com>
2025-09-07 19:46:04 +0800

    <feature>: reactFlow quickStartDemo

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,65 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useCallback, useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
+// import { Button } from 'antd';
+import { 
+  ReactFlow, 
+  applyNodeChanges, 
+  applyEdgeChanges, 
+  addEdge, 
+  type Node,
+  type Edge,
+  type OnNodesChange,
+  type OnEdgesChange,
+  type OnConnect
+} from '@xyflow/react';
+import '@xyflow/react/dist/style.css';
+
+const initNodes: Node[] = [
+  {id: 'n1', position: {x: 0, y: 0}, data: { label: 'Node 1'}},
+  {id: 'n2', position: {x: 0, y: 100}, data: { label: 'Node 2'}}
+];
+
+const initEdges: Edge[] = [{id: 'n1-n2', source: 'n1', target: 'n2'}]
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [nodes, setNodes] = useState<Node[]>(initNodes);
+  const [edges, setEdges] = useState<Edge[]>(initEdges);
+
+  const onNodesChange: OnNodesChange = useCallback(
+    (changes) => setNodes(
+      nodesSnapshot => applyNodeChanges(changes, nodesSnapshot)
+    ),
+    [setNodes]
+  );
+
+  const onEdgesChange: OnEdgesChange = useCallback(
+    (changes) => setEdges(
+      edgesSnapshot => applyEdgeChanges(changes, edgesSnapshot)
+    ),
+    [setEdges]
+  );
+
+  const onConnect: OnConnect = useCallback(
+    connection => setEdges(
+      edgesSnapshot => addEdge(connection, edgesSnapshot)
+    ),
+    [setEdges]
+  );
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div style={{width: '100vw', height: '100vh'}}>
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+      />
+    </div>
   )
+
 }
 
 export default App
