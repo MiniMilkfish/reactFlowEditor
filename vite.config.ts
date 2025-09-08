70169265dc7db398ff834eb04583ad91a45bbb4b
MiniMilkFish <minimilkfish@outlook.com>
2025-09-08 12:52:01 +0800

    <add>:dependencies add

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite';
+import path from "node:path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(), 
+    // tailwindcss()
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  },
+  css: {
+    postcss: './postcss.config.js',
+    // 预处理器选项（如果需要）
+    preprocessorOptions: {
+      // css: {},
+    },
+  },
 })
